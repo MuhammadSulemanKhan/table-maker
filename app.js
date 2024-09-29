aabc145aba28d320bacb67b6487bd32cf3a75d69
MuhammadSulemanKhan <muhammadsulemansk1256@gmail.com>
2024-09-29 21:42:53 +0500

    added

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var input = document.querySelector("#input")
 var printt = document.querySelector("#print")
 var btn = document.querySelector("#btn")
-
+var interval = 0
 function tablemaker(){
     printt.innerHTML=""
     
     number = input.value
     for(var i=1; i<=10; i++){
-        printt.innerHTML = printt.innerHTML +`${number} * ${i} = ${number*i}  <br>`
+        printt.innerHTML = printt.innerHTML +`${number}  *  ${i}  =  ${number*i}  <br /><br /> `
     }
+}
+
+function reset (){
+    // clearInterval(interval)
+    printt.innerHTML = ""
+    input.innerHTML=""
 }
